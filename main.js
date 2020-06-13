4f98c939af5e55538714b6866fbbb8c10b2978ef
jadergermano <jader.germano@icloud.com>
2020-06-13 03:33:11 -0300

    Formas de Referenciar tags da Dom QuerySelector por Propriedade

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Formas de Referenciar tags da Dom QuerySelector
-var inputElement = document.querySelector('div#app input')
+var inputElement = document.querySelector('input[name=nome]');
 
 
 console.log(inputElement);
+
