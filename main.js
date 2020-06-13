4305d70c01bff2391eca76c511866f4080dc9c83
jadergermano <jader.germano@icloud.com>
2020-06-13 04:12:38 -0300

    manipulação de elementos na DOM com js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let linkElement = document.createElement('a');
 linkElement.setAttribute('href','https://github.com/jader-germano/study');
+linkElement.setAttribute('title','Git do Jader');
 
 let textElement = document.createTextNode('Acesso ao repositório deste projeto.');
 linkElement.appendChild(textElement);
@@ -7,5 +8,5 @@ linkElement.appendChild(textElement);
 let containerElement = document.querySelector('#app');
 containerElement.appendChild(linkElement);
 
-/**
- *  <a href="https://github.com/jader-germano/study">Acesso ao repositório do projeto</a>*/
+let inputElement = document.querySelector('#nome');
+containerElement.removeChild(inputElement);
