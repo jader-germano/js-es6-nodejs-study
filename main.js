5b2b2a553b2d0e841b432b4124748d06d0b21a72
jadergermano <jader.germano@icloud.com>
2020-06-13 03:43:45 -0300

    Manipulando dom com QuerySelector

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 // Formas de Referenciar tags da Dom QuerySelector
-var inputElement = document.querySelector('input[name=nome]');
-
+var inputElement = document.querySelector('input');
+var allInputElements = document.querySelectorAll('input');
 
 console.log(inputElement);
+console.log(allInputElements);
+
+// Formas de Referenciar tags da Dom QuerySelector
+var btnElement = document.querySelector('button.botao');
+
+btnElement.onclick = function() {
+    let text = inputElement.value;
+    alert(text);
+}
+console.log(inputElement);
+console.log(allInputElements)
 
