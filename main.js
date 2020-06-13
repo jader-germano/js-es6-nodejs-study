9182c1f6d373291b10ffb5952384865d053046f5
jadergermano <jader.germano@icloud.com>
2020-06-13 03:27:19 -0300

    Formas de Referenciar tags da Dom

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function mostrarAlerta() {
-        alert('Botão foi clicado!')
-}
+// Formas de Referenciar tags da Dom
+var elementById = document.getElementById('nome');
+
+console.log(elementById);
+
+var elementsByTagName = document.getElementsByTagName('input')[0];
+console.log(elementsByTagName);
+
+var elementsByClassName= document.getElementsByClassName('nome')[0];
+console.log(elementsByClassName);
