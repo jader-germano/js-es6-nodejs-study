b5276c9264ae9cea53192443cc25d01786c75ae6
jadergermano <jader.germano@icloud.com>
2020-06-14 09:48:56 -0300

    Utlizando biblioteca axios para facilitar a utilização de requisições nativas do js pelo XMLHttpRequest

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-let promise = function () {
-
-    return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
-
-        xhr.open('GET', 'https://api.github.com/users/jader-germano');
-        xhr.send(null);
-        xhr.onreadystatechange = function () {
-            // 4 = resposta pronta
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('Erro na requisição.')
-                }
-            }
-        }
-    });
-}
-promise()
+axios.get('https://api.github.com/users/jader-germano')
     .then(function (response) {console.log(response)})
     .catch(function (error) {console.error(error)});
 
