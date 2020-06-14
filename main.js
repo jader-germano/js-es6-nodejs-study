ed8d6356afca4278c616778bde409856c4d0ac13
jadergermano <jader.germano@icloud.com>
2020-06-14 13:09:48 -0300

    exmplos de operações com vetores em ES6

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-class Matematica {
-    static sum(a,b) {
-        return a + b;
-    }
-}
-console.log(Matematica.sum(1,2))
+const arr = [1, 3, 4, 5, 8, 9];
+
+const newArr = arr.map((item, index) => item + index);
+
+console.log(newArr);
+
+const sum = arr.reduce((total, next) => total + next);
+
+console.log(sum);
+
+const filter = arr.filter((item) => item % 2 === 0);
+
+console.log(filter);
+
+const find = arr.find((item) => item === 2);
+
+console.log(find);
