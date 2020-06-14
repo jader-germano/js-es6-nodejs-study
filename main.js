c5a3711beada016d41657512c3e634b8fd1ed4f1
jadergermano <jader.germano@icloud.com>
2020-06-14 15:12:16 -0300

    addition of polyfills and async/await

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-import ClassUSer, { idade } from './user';
+const promise = () => new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('OK')}, 2000)
+})
 
-ClassUSer.information();
-console.log(idade)
-ClassUSer.idade = 25;
-console.log(ClassUSer.idade)
+/*
+ promise().then(response => {
+ console.log(response)
+ }).catch(err => err);
+ */
+async function execute() {
+    const response = await promise();
+    console.log(response)
+}
+
+execute()
--- user.js
+++
@@ -1,6 +0,0 @@
-export const idade = 23;
-export default class User {
-    static information() {
-        console.log('Apenas teste');
-    }
-}
