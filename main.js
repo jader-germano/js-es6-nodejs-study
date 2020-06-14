0beb260dc17a57c2210c349ffd5d6dd7d6fd863a
jadergermano <jader.germano@icloud.com>
2020-06-14 14:02:13 -0300

    Configuring webpack for running the project with babel

--- main.js
+++ main.js
@@ -1,47 +1,23 @@
-//REST
-const user = {
-    name: 'Jader',
-    age: '27',
-    address: {
-        city: 'Brasília',
-        estate: 'DF',
+class User {
+    constructor(mail, pass) {
+        this.mail = mail;
+        this.password = pass;
+        this.admin = false;
     }
-};
+    isAdmin() {
+        return this.admin;
+    }
 
-const { name, ...restUSer } = user;
-console.log(name);
-console.log(restUSer);
+}
+class Admin extends User{
+    constructor() {
+        super();
+        this.admin = true;
+    }
 
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-console.log(a);
-console.log(b);
-console.log(c);
-
-function sum(a, ...params) {
-    return a + params.reduce((total, next) => total + next);
 }
 
-console.log(sum(1, 2, 3, 4, 5, 6, 7, 8));
-
-// SPREAD
-
-const arr1 = [1, 2, 3, 4];
-const arr2 = [5, 6, 7, 8, 9];
-
-const arr3 = [...arr, ...arr1, ...arr2];
-console.log(arr3);
-
-const user2 = {
-    name: 'Philipe',
-    age: '27',
-    address: {
-        city: 'Brasília',
-        estate: 'DF',
-    }
-};
-
-const user3 = {...user2, name: 'Germano'};
-
-console.log(user3);
+const User1 = new User('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+console.log(User1.isAdmin()) // false
+console.log(Adm1.isAdmin()) // true
