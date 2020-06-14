5c1fde9198e62226c2760e196967d4ea97b24397
jadergermano <jader.germano@icloud.com>
2020-06-13 21:33:07 -0300

    Exercícos de manipulção simples da DOM

--- main.js
+++ main.js
@@ -1,48 +1,55 @@
-var nomes = ["Diego", "Gabriel", "Lucas"];
+var nomes = ["Diego", "Jader", "amendoim", "Lucas"];
 
 let appElement = document.querySelector('#app');
 
-async function iterarLista() {
-    let ulElement = document.getElementsByTagName('ul');
+let inputElement = document.createElement('input');
 
-    for (const appElementElement of ulElement) {
-        console.log(appElementElement);
-        appElement.removeChild(appElementElement);
-    }
+let btnElement = document.createElement('button');
 
+async function loadList() {
+    let ulElements = document.querySelector('#list');
+    ulElements && appElement.removeChild(ulElements);
+
+    let itemUlElement = document.createElement('ul');
+    itemUlElement.setAttribute('id', 'list');
     for (let nome of nomes) {
-        let itemUlElement = document.createElement('ul');
+        let itemLiElement = document.createElement('li');
         let textElement = document.createTextNode(nome);
-        itemUlElement.appendChild(textElement);
-        await appElement.appendChild(itemUlElement)
+        itemLiElement.appendChild(textElement);
+        itemUlElement.appendChild(itemLiElement);
+        appElement.appendChild(itemUlElement);
     }
 }
 
+function createButton() {
+    inputElement.setAttribute('text', 'text');
+    inputElement.setAttribute('name', 'name');
+    btnElement.style.width = 90;
+    btnElement.style.color = '#3e3c3c';
+    btnElement.style.height = 30;
+    btnElement.style.padding = 15;
+    btnElement.style.borderStyle = 'none';
+    btnElement.style.margin = 20;
+    btnElement.style.borderRadius = '15%';
+    btnElement.style.backgroundColor = '#d4cfcf';
+    let textInputElement = document.createTextNode('Insira');
+    btnElement.appendChild(textInputElement);
+}
 
-let inputElement = document.createElement('input');
-inputElement.setAttribute('text', 'text');
-inputElement.setAttribute('name', 'name');
-
-let btnElement = document.createElement('button');
-btnElement.style.width = 90;
-btnElement.style.color = '#3e3c3c';
-btnElement.style.height = 30;
-btnElement.style.padding = 15;
-btnElement.style.borderStyle = 'none';
-btnElement.style.margin = 20;
-btnElement.style.borderRadius = '15%';
-btnElement.style.backgroundColor = '#d4cfcf';
 btnElement.onclick = async function () {
+
     if (!inputElement.value) {
         alert('Preencha o campo de texto');
         return
     }
     nomes = [...nomes, inputElement.value];
     inputElement.value = '';
-    await iterarLista();
+    await loadList();
 }
 
 appElement.appendChild(inputElement);
+
 appElement.appendChild(btnElement);
 
-iterarLista();
+loadList();
+createButton();
