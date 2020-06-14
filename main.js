22f9718b5ed68b106d3dc8291fd971b26171f657
jadergermano <jader.germano@icloud.com>
2020-06-14 14:30:06 -0300

    exemplification of usage and gains of webpack

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-class User {
-    constructor(mail, pass) {
-        this.mail = mail;
-        this.password = pass;
-        this.admin = false;
-    }
-    isAdmin() {
-        return this.admin;
-    }
+import  {sum} from './functions'
 
-}
-class Admin extends User{
-    constructor() {
-        super();
-        this.admin = true;
-    }
-
-}
-
-const User1 = new User('email@teste.com', 'senha123');
-const Adm1 = new Admin('email@teste.com', 'senha123');
-console.log(User1.isAdmin()) // false
-console.log(Adm1.isAdmin()) // true
+console.log(sum(1, 2))
